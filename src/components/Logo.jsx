27806939dbd821e1344d7d27fe29a6cf15a94736
harshjainh2j <141403634+harshjainh2j@users.noreply.github.com>
2025-07-02 15:44:22 +0530

    logo fix

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Logo = ({ width = '100px' }) => {
   return (
     <div style={{ width }}>
-      <img src="/src/assets/logo.png" alt="logo" style={{ width: '100%' }} />
+      <img src="/logo.png" alt="logo" style={{ width: '100%' }} />
     </div>
   )
 }
